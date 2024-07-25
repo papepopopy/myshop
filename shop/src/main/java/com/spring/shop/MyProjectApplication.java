20cfecf6284eb7b932339f87d15759ad80948d1a
papepopopy <168069683+papepopopy@users.noreply.github.com>
2024-07-25 19:06:40 +0900

    Delete shop directory

--- shop/src/main/java/com/spring/shop/MyProjectApplication.java
+++
@@ -1,9 +0,0 @@
-package com.spring.shop;
-
-import org.springframework.boot.SpringApplication;
-
-public class MyProjectApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MyProjectApplication.class, args);
-	}
-}
--- shop/src/main/java/com/spring/shop/constant/ItemSellStatus.java
+++
@@ -1,6 +0,0 @@
-package com.spring.shop.constant;
-
-public enum ItemSellStatus {
-	SELL, SOLID_OUT
-	
-}
